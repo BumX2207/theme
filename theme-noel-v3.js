7438932ef05ba8ceb680f5dbc2a0113adfd648ae
BumX2207 <congtunho209@gmail.com>
2026-04-27 13:55:17 +0700

    Update theme-noel-v3.js

--- theme-noel-v3.js
+++ theme-noel-v3.js
@@ -40,17 +40,17 @@
         body.glass-ui-mode .nav-icon-circle::before, .nav-icon-circle::before {
             content: '';
             position: absolute;
-            top: -35px; 
-            right: -30px;
-            width: 65px; 
-            height: 65px;
+            top: -30px;
+            right: -24px;
+            width: 55px;
+            height: 55px;
             /* Cục mã SVG siêu việt nằm luôn trong CSS */
             background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'%3E%3Cpath fill='%23E63946' d='M421.3 285.8c-12-32.8-37.4-87.3-68.5-125.5C311.2 109 256 64 256 64S171.1 113.8 119.5 175.7c-33 39.8-59 86-63.5 94.6-6.1 11.6-9.6 24.6-10.2 38-.9 17.5 3.3 34.6 11.8 49 14.5 24.5 40.5 38.6 68.4 38.6h260c27.9 0 53.9-14.1 68.4-38.6 8.5-14.4 12.7-31.5 11.8-49-.5-11.4-2.8-22.5-7.3-32.5z'/%3E%3Cpath fill='%23FFFFFF' d='M443.5 365H68.5C50 365 35 380 35 398.5S50 432 68.5 432h375c18.5 0 33.5-15 33.5-33.5S462 365 443.5 365z'/%3E%3Ccircle fill='%23FFFFFF' cx='93.2' cy='151.7' r='60'/%3E%3C/svg%3E") !important; 
             background-size: contain !important;
             background-repeat: no-repeat !important;
             background-position: center !important;
             z-index: 100 !important;
-            transform: rotate(18deg);
+            transform: rotate(35deg);
             pointer-events: none;
             filter: drop-shadow(0px 3px 3px rgba(0,0,0,0.4)) !important; 
         }
